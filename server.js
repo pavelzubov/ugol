fddaa3878f242709201aca5f79f697bb9d549cb3
pasha224@yandex.ru <pasha1990>
2018-06-01 15:45:31 +0300

    fix

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('/dist/ugol'));
 
-app.get('/*', function(req,res) {
+app.get('/*all', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+/*app.get('/!*', function(req,res) {
 
   res.sendFile(path.join('/dist/ugol/index.html'));
-});
+});*/
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
